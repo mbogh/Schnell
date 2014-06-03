9cc1c3118eff3c77e6fa2e7b977a6b476ec03089
Morten Bøgh <mob@trifork.com>
2014-06-03 22:08:40 +0200

    Initial Swift Xcode commit

---
+++ Schnell/Schnell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schnell
+//
+//  Created by Morten Bøgh on 03/06/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
