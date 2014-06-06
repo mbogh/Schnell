80dfa20d837bdcd7df390295524bfa69675b0926
Morten Bøgh <mob@trifork.com>
2014-06-06 19:16:12 +0200

    Basic API class setup.

---
+++ Schnell/Models/OD3API.swift
@@ -0,0 +1,21 @@
+//
+//  OD3API.swift
+//  Schnell
+//
+//  Created by Morten Bøgh on 06/06/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import Foundation
+
+class OD3API {
+    var urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    
+    func loadRoadSections() {
+        
+    }
+    
+    func loadRoadSectionDetails() {
+        
+    }
+}
