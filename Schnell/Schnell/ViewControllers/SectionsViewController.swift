e7a9f830a5150f2850e3c497bdb841bda448ac15
Morten Bøgh <mob@trifork.com>
2014-06-06 18:42:45 +0200

    Have decided to use the speed data set from ODAA in this example app.

---
+++ Schnell/Schnell/ViewControllers/SectionsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SectionsViewController.swift
+//  Schnell
+//
+//  Created by Morten Bøgh on 05/06/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import UIKit
+
+class SectionsViewController: UITableViewController {
+    var sections = ["","","",""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    // #pragma mark - UITableViewDataSource
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sections.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SectionCell") as UITableViewCell
+
+        return cell
+    }
+}
