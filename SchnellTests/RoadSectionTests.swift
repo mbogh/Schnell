cee848a1bbfaf5d9315d41e28190c2fdfcafe6e0
Morten Bøgh <mob@trifork.com>
2014-06-13 21:34:50 +0200

    Added more test cases for the computed var name on RoadSection

--- SchnellTests/RoadSectionTests.swift
+++ SchnellTests/RoadSectionTests.swift
@@ -23,4 +23,32 @@ class RoadSectionTests: XCTestCase {
         
         XCTAssertEqual(roadSection.name, "Test Road 1 - 42", "Smallest number should be first when it is the same street")
     }
+    
+    func testSameStreetFirstZeroStreetNumber() {
+        let data = NSDictionary(objects: [165, "Test Road", "0", "Test Road", "42"], forKeys:["_id", "POINT_1_STREET", "POINT_1_STREET_NUMBER", "POINT_2_STREET", "POINT_2_STREET_NUMBER"])
+        let roadSection = RoadSection(data: data)
+        
+        XCTAssertEqual(roadSection.name, "Test Road 1 - 42", "Street number 0 should be converted to a 1.")
+    }
+    
+    func testSameStreetSecondZeroStreetNumber() {
+        let data = NSDictionary(objects: [165, "Test Road", "42", "Test Road", "0"], forKeys:["_id", "POINT_1_STREET", "POINT_1_STREET_NUMBER", "POINT_2_STREET", "POINT_2_STREET_NUMBER"])
+        let roadSection = RoadSection(data: data)
+        
+        XCTAssertEqual(roadSection.name, "Test Road 1 - 42", "Street number 0 should be converted to a 1.")
+    }
+    
+    func testPoint1StreetNil() {
+        let data = NSDictionary(objects: [165, "1", "Test Road", "42"], forKeys:["_id", "POINT_1_STREET_NUMBER", "POINT_2_STREET", "POINT_2_STREET_NUMBER"])
+        let roadSection = RoadSection(data: data)
+        
+        XCTAssertEqual(roadSection.name, "Test Road 1 - 42", "A nil Street name is considered a 1 street road section.")
+    }
+    
+    func testPoint2StreetNil() {
+        let data = NSDictionary(objects: [165, "1", "Test Road", "42"], forKeys:["_id", "POINT_1_STREET_NUMBER", "POINT_1_STREET", "POINT_2_STREET_NUMBER"])
+        let roadSection = RoadSection(data: data)
+        
+        XCTAssertEqual(roadSection.name, "Test Road 1 - 42", "A nil Street name is considered a 1 street road section.")
+    }
 }
