44ad2215eb0b2b04f3cfef5fc9a93083a3d8d99a
Morten Bøgh <mob@trifork.com>
2014-06-19 23:25:11 +0200

    Added deselect in the didSelect delegate method. Xcode 6 b2 autocomplete is still broken.

--- Schnell/ViewControllers/SectionsViewController.swift
+++ Schnell/ViewControllers/SectionsViewController.swift
@@ -42,4 +42,8 @@ class SectionsViewController: UITableViewController {
     }
     
     // #pragma mark - UITableViewDelegate
+    
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
