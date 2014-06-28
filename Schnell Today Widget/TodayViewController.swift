7416cf6a59437ba1169e725807d0e8c354151d19
Morten Bøgh <mob@trifork.com>
2014-06-28 22:10:15 +0200

    Added initial Today widget

---
+++ Schnell Today Widget/TodayViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TodayViewController.swift
+//  Schnell Today Widget
+//
+//  Created by Morten Bøgh on 28/06/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController {
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encoutered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+}
