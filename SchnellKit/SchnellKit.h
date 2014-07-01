e8ea0d347f814d388abe29a6f01851ddb3d467bf
Morten Bøgh <mob@trifork.com>
2014-07-01 22:32:22 +0200

    Need to stop watching WWDC videos which gives me new ideas :)

---
+++ SchnellKit/SchnellKit.h
@@ -0,0 +1,19 @@
+//
+//  SchnellKit.h
+//  SchnellKit
+//
+//  Created by Morten Bøgh on 01/07/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SchnellKit.
+FOUNDATION_EXPORT double SchnellKitVersionNumber;
+
+//! Project version string for SchnellKit.
+FOUNDATION_EXPORT const unsigned char SchnellKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SchnellKit/PublicHeader.h>
+
+
